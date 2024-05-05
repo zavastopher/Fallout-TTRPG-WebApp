297d533cb6620ffc968f734f71e1cd52b20a19f1
Chris Zavala <zavastopher@gmail.com>
2024-05-05 08:29:36 -0400

    Dockerized Fronted and Backend Successfully

--- backend/vault-36-server.py
+++ backend/vault-36-server.py
@@ -1,4 +1,8 @@
 from flask import Flask
+
+# This import is importing all of the Flask Configurations deffined in config.py
+from config import *
+
 app = Flask(__name__)
 
 @app.route('/')
@@ -6,4 +10,4 @@ def hello_geek():
     return '<h1>Hellow from Vault 36<h1>'
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, host=HOST, port=3001)
